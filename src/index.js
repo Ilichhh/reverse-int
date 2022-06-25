93fcf57476bc28db84b96d6566446526f5c71ff2
ilya <shakurov.ia@gmail.com>
2022-06-25 16:57:14 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+    let result = '';
+    const strNumber = Math.abs(n).toString();
   
+    for (let i=strNumber.length - 1; i >= 0; i--) {
+      result += strNumber[i];
+    }
+
+    return result;
 }
